e26523214f89b5906fb4eda31f0c4c3bb73e3dff
Gwilym Kuiper <gw@ilym.me>
2018-06-03 17:04:25 +0100

    Add own player to wait list

--- ServerWait.gd
+++ ServerWait.gd
@@ -3,12 +3,13 @@ extends Control
 func _ready():
 	var tree = get_tree()
 	tree.connect("network_peer_connected", self, "new_peer")
+	new_peer(tree.get_meta("network_peer").get_unique_id())
 	
 	if tree.is_network_server():
 		$StartGameButton.visible = true
 
 func start_game():
-	pass
+	get_tree().set_refuse_new_network_connections(true)
 
 func new_peer(id):
 	$PlayersConnected.add_item("Player: " + str(id))

aa5dee1800ffcb79c2a4b0d19486e86913196b69
Corwin Kuiper <8143879+yodamerlin@users.noreply.github.com>
2018-06-04 15:05:32 +0100

    Fixed rotation direction

--- TilePlacement.gd
+++ TilePlacement.gd
@@ -32,7 +32,7 @@ func _input(event):
 			$TileDraggable.visible = false
 			emit_signal("placeTile", tileID, orientation, event.position.x, event.position.y)
 	if event.is_action_pressed("rotate_left"):
-		orientation = (orientation + 1) % 4
-	if event.is_action_pressed("rotate_right"):
 		orientation = (orientation + 3) % 4
+	if event.is_action_pressed("rotate_right"):
+		orientation = (orientation + 1) % 4
 	$TileDraggable.rotation = orientation * PI/2

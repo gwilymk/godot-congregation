5b23e2367f07f19e45cebc37329ab9008ee86089
unknown <8143879+yodamerlin@users.noreply.github.com>
2018-06-18 02:28:51 +0100

    Added Watchtower increasing range

--- Lighting.gd
+++ Lighting.gd
@@ -5,10 +5,14 @@ onready var my_player_id = get_tree().get_meta("network_peer").get_unique_id()
 onready var tile_size = get_parent().get_node("Map").TILE_SIZE
 onready var map_size = Vector2(get_parent().get_node("Map").width, get_parent().get_node("Map").height)
 
+onready var Map = get_parent().get_node("Map")
+
 export var VIEW = 4
+export var WATCHTOWER = 6
 
 func _process(delta):
 	LightSquares = []
+	
 	
 	var followers = get_parent().get_node("Followers").get_children()
 	
@@ -17,13 +21,18 @@ func _process(delta):
 			var posExact = follower.position / tile_size
 			var posRound = Vector2(floor(posExact.x), floor(posExact.y))
 			var posIn = posExact-posRound
-			for x in range(-4,4):
-				for y in range(-4,4):
+			
+			var visibleRange
+			if Map.tiles[Map.tile_id(posRound.x, posRound.y)].has_watchtower():
+				visibleRange = WATCHTOWER
+			else:
+				visibleRange = VIEW
+			for x in range(-visibleRange,visibleRange):
+				for y in range(-visibleRange,visibleRange):
 					var xDif = posIn.x + x 
 					var yDif = posIn.y + y
-					if xDif*xDif + yDif*yDif < VIEW*VIEW:
+					if xDif*xDif + yDif*yDif < visibleRange*visibleRange:
 						LightSquares.push_back(posRound + Vector2(x,y))
-						
 	update()
 
 func _draw():	
